560829c8588e48d552790ae7ffa10cddefad761e
antoine frau <antoine.frau1108@gmail.com>
2019-02-28 22:44:12 +0100

    :tada: WebPack configuration add bootstrap

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,7 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-
+mix.options({ extractVueStyles: true });
 mix.js('resources/assets/js/app.js', 'public/js')
+   .js('resources/assets/js/bootstrap.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
