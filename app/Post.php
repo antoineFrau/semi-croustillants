c297af0c44eedfe3ad0094e561ae088650d530cb
antoine frau <antoine.frau1108@gmail.com>
2019-04-22 15:51:20 +0200

    :hammer: Refactor Models

--- app/Post.php
+++ app/Post.php
@@ -10,7 +10,7 @@ class Post extends Model
      * Post belongs to one User
      * posts.user_id  ->  users.id
      */
-    public function customer()
+    public function user()
     {
         return $this->belongsTo('App\User', 'user_id');
     }
